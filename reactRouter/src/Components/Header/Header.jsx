340ed640d77b6445991760478537c68f5171dcc2
Banty kumar <banty0163@gmail.com>
2025-07-24 00:11:07 +0530

    header-update

--- reactRouter/src/Components/Header/Header.jsx
+++ reactRouter/src/Components/Header/Header.jsx
@@ -4,7 +4,7 @@ import {Link,NavLink} from 'react-router-dom'
 function Header() {
   return (
       <header className="shadow sticky z-50 top-0">
-            <h1>banty kumar vishwakarma</h1>
+         
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
