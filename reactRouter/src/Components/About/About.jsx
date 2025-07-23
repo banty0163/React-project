5a888b0522bcb4c2c133a21dfe9c1c578d9fa16d
Banty kumar <143025724+banty0163@users.noreply.github.com>
2025-07-23 23:54:30 +0530

    Update About.jsx

--- reactRouter/src/Components/About/About.jsx
+++ reactRouter/src/Components/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
                         <img
-                            src="https://tailus.io/sources/blocks/left-image/preview/images/startup.png"
+                            src="https://cdn.pixabay.com/photo/2016/06/29/16/36/logo-1487245_1280.png"
                             alt="image"
                         />
                     </div>
